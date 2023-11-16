9d1a83aec075869008734d9765e6149e47d500f4
MaximilianHolleran <mhholleran@gmail.com>
2023-11-15 22:39:37 -0500

    activity complete could not figure out bonus for now

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (asset, dirLeft, dirBottom) {
+    let icon = document.createElement('img')
+    icon.src = asset
+    icon.style.position = 'fixed'
+    icon.style.left = dirLeft
+    icon.style.bottom = dirBottom
+    document.body.append(icon)
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pine-tree.png', '450px', '200px')
+
+
+function newItem (asset, dirLeft, dirBottom){
+    let weapon = document.createElement('img')
+    weapon.src = asset
+    weapon.style.position = 'fixed'
+    weapon.style.left = dirLeft
+    weapon.style.bottom = dirBottom
+    document.body.append(weapon)
+    weapon.addEventListener('dblclick', function(){
+        weapon.remove()})
+}
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
+
