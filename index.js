8a4cda9de3b9a4923fcca3e2f946c9d4f5bdfa69
MaximilianHolleran <mhholleran@gmail.com>
2023-11-17 13:37:52 -0500

    got everything working correctly for first class citizens

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 let inventory = document.createElement('div')
 
-function move(image, left, bottom){
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-}
-
 function newImage (asset) {
     let icon = document.createElement('img')
     icon.src = asset
-    /*icon.style.position = 'fixed'*/
-    /*icon.style.left = dirLeft*/
-    /*icon.style.bottom = dirBottom*/
     document.body.append(icon)
     return icon
 }
@@ -27,49 +18,51 @@ function move(image){
     return {
         to: moveToCoordinates
     }
-
-    move(greenCharacter, 100, 250)
 }
 
-let greenCharacter = newImage('assets/green-character.gif')
-
-/*move(greenCharacter, 100, 250)*/
-
-function newItem (asset, dirLeft, dirBottom){
-    let weapon = newImage(asset, dirLeft, dirBottom)  
-    document.body.append(weapon)
+function newItem (asset){
+    let weapon = newImage(asset)  
     weapon.addEventListener('dblclick', function(){
-    weapon.remove()
-    let inventoryItem = document.createElement('img')
-    inventoryItem.src = asset
-    inventory.append(inventoryItem)
+        weapon.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = asset
+        inventory.append(inventoryItem)
 })
+return weapon
 }
 
-/*function createBackground(){
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    let grassHeight = height*.6;
-    let skyHeight = height*.4;
-    for(let i=0; i<width; i+=100){
-       for(let j=0; j<grassHeight; j+=100){
-    newImage('assets/grass.png', i+ 'px', j + 'px');
-       }
-     }
-    for(let i=0; i<width; i+=100){
-      for(let j=0; j<skyHeight; j+=100){
-        let k = grassHeight + j;
-        newImage('assets/sky.png', i + 'px', k + 'px');
-      }
-     }
+function backgroundImage(asset, left, bottom){
+    let image = document.createElement('img')
+    image.src = asset
+    image.style.position = 'fixed'
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+}
+
+function createBackground(){
+    let grassHeight = window.innerHeight *.69;
+    let skyHeight = window.innerHeight *.31;
+
+    for(let i=0; i<window.innerWidth; i+=100){
+        for(let j=0; j<grassHeight; j+=100){
+            backgroundImage('assets/grass.png', i+ 'px', j + 'px');
+        }
     }
+
+      for(let i=0; i<window.innerWidth; i+=100){
+        for(let j=0; j < skyHeight; j+=100){
+          let k = grassHeight + j;
+            backgroundImage('assets/sky.png', i + 'px', k + 'px');
+        }
+    }
+}
     createBackground();
-*/
+
 
     function newInventory(){
-
         inventory.style.position = 'fixed'
-        inventory.style.bottom = '0px';
+        inventory.style.bottom = '0px'
         inventory.style.left = '0px'
         inventory.style.width = '100%'
         inventory.style.height = '100px'
@@ -83,16 +76,16 @@ function newItem (asset, dirLeft, dirBottom){
     }
 
     newInventory()
-    /*newImage('assets/green-character.gif', '100px', '100px')*/
-    newImage('assets/pillar.png', '350px', '100px')
-    newImage('assets/crate.png', '150px', '200px')
-    newImage('assets/well.png', '500px', '425px')
-    newImage('assets/tree.png', '200px', '300px')
-    newImage('assets/pine-tree.png', '450px', '200px')
-    
-    newItem('assets/sword.png', '500px', '405px')
-    newItem('assets/shield.png', '165px', '185px')
-    newItem('assets/staff.png', '600px', '100px')
+    move(newImage('assets/green-character.gif')).to(100,250)
+    move(newImage('assets/tree.png')).to(200, 450)
+    move(newImage('assets/pillar.png')).to(350, 250)
+    move(newImage('assets/pine-tree.png')).to(450, 350)
+    move(newImage('assets/crate.png')).to(150, 350)
+    move(newImage('assets/well.png')).to(500, 575)
+
+    move(newItem('assets/sword.png')).to(500, 555)
+    move(newItem('assets/shield.png')).to(165, 335)
+    move(newItem('assets/staff.png')).to(600, 250)
 
 
 
